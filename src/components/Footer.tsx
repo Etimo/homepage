6b10c314ce4c105ac7bf91c6c1fee6929bb314ac
Alfred Stenborg <alfst109@student.liu.se>
2020-06-17 14:11:15 +0200

    Prep for 'Kontakt' section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import Section from './Section';
 
 const FooterComponent = styled.footer`
 	width: 100%;
@@ -28,6 +29,7 @@ const Icon = styled(FontAwesomeIcon)`
 	}
 `;
 
+/** UNUSED */
 const Triangle = styled.div`
 	width: 0;
 	height: 0;
@@ -38,6 +40,7 @@ const Triangle = styled.div`
 
 const Footer = () => {
 	return (
+		/*<Section style={{ backgroundColor: '#231f20' }}>*/
 		<FooterComponent>
 			<ul>
 				<li>
@@ -60,7 +63,21 @@ const Footer = () => {
 				</li>
 			</ul>
 		</FooterComponent>
+		/*</Section>*/
 	);
 };
+
+/* Caption(vit) - Kontakt
+	H2/EmphasizedH2(vit) - Kontakta oss
+	Caption(vit) - Telefon
+	P(vit) - nummer
+	Caption(vit) - E-post
+	P(vit) - e-post
+	Caption(vit) - Adress
+	P(vit) - Adress
+	P(vit) - https://www.google.com/maps/place/Etimo/@59.3341074,18.0564272,17z/data=!3m1!4b1!4m5!3m4!1s0x465f9d456a3a308f:0xb48135247622ea68!8m2!3d59.3341074!4d18.0586159
+	Ikoner
+
+*/
 
 export default Footer;
