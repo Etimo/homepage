f1d23ad9737395ac234eddcee25263177f8abb98
Alfred Stenborg <astenbor@hotmail.com>
2020-07-08 13:52:55 +0200

    Added more customers to /klienter

--- src/components/Clients/ClientCustomers.tsx
+++ src/components/Clients/ClientCustomers.tsx
@@ -1,24 +1,14 @@
 import { graphql, useStaticQuery } from 'gatsby';
 import Img from 'gatsby-image';
 import React from 'react';
-import styled from 'styled-components';
-import tw from 'twin.macro';
-import { FloatInDir, FloatUp } from '../animations';
-import { AnimatedH2, Caption, Span } from '../elements';
-import Section from './Section';
+import Customers from '../Customers';
 
-/* max-width: 150px; */
-const ImageDiv = styled.div`
-	${tw`mx-auto w-full max-w-xxxxxs md:max-w-xxxxs xl:max-w-xxxs opacity-50 hover:opacity-100`};
-	${tw`transition-opacity ease-in-out duration-200`};
-`;
-
-const generateCustomers = () => {
+const ClientCustomers = () => {
 	const data = useStaticQuery(graphql`
 		query {
 			tele2: file(relativePath: { eq: "customers/modified/tele2.png" }) {
 				childImageSharp {
-					fluid(maxWidth: 500, quality: 100) {
+					fluid(maxWidth: 500, quality: 90) {
 						...GatsbyImageSharpFluid_tracedSVG
 					}
 				}
@@ -32,49 +22,80 @@ const generateCustomers = () => {
 			}
 			qliro: file(relativePath: { eq: "customers/modified/qliro.png" }) {
 				childImageSharp {
-					fluid(maxWidth: 500, quality: 100) {
+					fluid(maxWidth: 500, quality: 90) {
 						...GatsbyImageSharpFluid_tracedSVG
 					}
 				}
 			}
 			karma: file(relativePath: { eq: "customers/modified/karma.png" }) {
 				childImageSharp {
-					fluid(maxWidth: 500, quality: 100) {
+					fluid(maxWidth: 500, quality: 90) {
 						...GatsbyImageSharpFluid_tracedSVG
 					}
 				}
 			}
 			mira: file(relativePath: { eq: "customers/modified/mira.png" }) {
 				childImageSharp {
-					fluid(maxWidth: 500, quality: 100) {
+					fluid(maxWidth: 500, quality: 90) {
 						...GatsbyImageSharpFluid_tracedSVG
 					}
 				}
 			}
 			nobina: file(relativePath: { eq: "customers/modified/nobina.png" }) {
 				childImageSharp {
-					fluid(maxWidth: 500, quality: 100) {
+					fluid(maxWidth: 500, quality: 90) {
 						...GatsbyImageSharpFluid_tracedSVG
 					}
 				}
 			}
 			quinyx: file(relativePath: { eq: "customers/modified/quinyx.png" }) {
 				childImageSharp {
-					fluid(maxWidth: 500, quality: 100) {
+					fluid(maxWidth: 500, quality: 90) {
 						...GatsbyImageSharpFluid_tracedSVG
 					}
 				}
 			}
 			seb: file(relativePath: { eq: "customers/modified/seb.png" }) {
 				childImageSharp {
-					fluid(maxWidth: 500, quality: 100) {
+					fluid(maxWidth: 500, quality: 90) {
+						...GatsbyImageSharpFluid_tracedSVG
+					}
+				}
+			}
+			megger: file(relativePath: { eq: "customers/modified/megger.png" }) {
+				childImageSharp {
+					fluid(maxWidth: 500, quality: 90) {
+						...GatsbyImageSharpFluid_tracedSVG
+					}
+				}
+			}
+			comviq: file(relativePath: { eq: "customers/modified/comviq.png" }) {
+				childImageSharp {
+					fluid(maxWidth: 500, quality: 90) {
+						...GatsbyImageSharpFluid_tracedSVG
+					}
+				}
+			}
+			appva: file(relativePath: { eq: "customers/modified/appva.png" }) {
+				childImageSharp {
+					fluid(maxWidth: 500, quality: 90) {
+						...GatsbyImageSharpFluid_tracedSVG
+					}
+				}
+			}
+			expressen: file(
+				relativePath: { eq: "customers/modified/expressen.png" }
+			) {
+				childImageSharp {
+					fluid(maxWidth: 500, quality: 90) {
 						...GatsbyImageSharpFluid_tracedSVG
 					}
 				}
 			}
 		}
 	`);
-	return [
+
+	const customers = [
 		{
 			name: 'Blocket',
 			url: 'https://www.blocket.se/',
@@ -121,7 +142,7 @@ const generateCustomers = () => {
 			image: <Img fluid={data.qliro.childImageSharp.fluid} />,
 			borders: {
 				xs: 'border-r border-b',
-				sm: 'lg:border-b-0 ',
+				sm: '',
 			},
 			direction: 'right',
 		},
@@ -131,14 +152,54 @@ const generateCustomers = () => {
 			image: <Img fluid={data.seb.childImageSharp.fluid} />,
 			borders: {
 				xs: 'border-b',
-				sm: 'lg:border-b-0 lg:border-r',
+				sm: 'lg:border-r',
 			},
-			direction: 'up',
+			direction: 'right',
 		},
 		{
 			name: 'Quinyx',
 			url: 'https://www.quinyx.com/sv/',
 			image: <Img fluid={data.quinyx.childImageSharp.fluid} />,
+			borders: {
+				xs: 'border-r border-b',
+				sm: '',
+			},
+			direction: 'left',
+		},
+		{
+			name: 'Nobina',
+			url: 'https://www.nobina.com/sv/Sverige/',
+			image: <Img fluid={data.nobina.childImageSharp.fluid} />,
+			borders: {
+				xs: 'border-b',
+				sm: '',
+			},
+			direction: 'left',
+		},
+		{
+			name: 'Megger',
+			url: 'https://se.megger.com/',
+			image: <Img fluid={data.megger.childImageSharp.fluid} />,
+			borders: {
+				xs: 'border-r border-b',
+				sm: 'lg:border-b-0',
+			},
+			direction: 'right',
+		},
+		{
+			name: 'Comviq',
+			url: 'https://www.comviq.se/',
+			image: <Img fluid={data.comviq.childImageSharp.fluid} />,
+			borders: {
+				xs: 'border-b',
+				sm: 'lg:border-b-0 lg:border-r',
+			},
+			direction: 'up',
+		},
+		{
+			name: 'Appva',
+			url: 'https://appva.com/sv/',
+			image: <Img fluid={data.appva.childImageSharp.fluid} />,
 			borders: {
 				xs: 'border-r',
 				sm: '',
@@ -146,9 +207,9 @@ const generateCustomers = () => {
 			direction: 'up',
 		},
 		{
-			name: 'Nobina',
-			url: 'https://www.nobina.com/sv/Sverige/',
-			image: <Img fluid={data.nobina.childImageSharp.fluid} />,
+			name: 'Expressen',
+			url: 'https://www.expressen.se/',
+			image: <Img fluid={data.expressen.childImageSharp.fluid} />,
 			borders: {
 				xs: '',
 				sm: '',
@@ -156,59 +217,12 @@ const generateCustomers = () => {
 			direction: 'left',
 		},
 	];
-};
-
-type Props = {
-	givenCustomers?: {
-		name: string;
-		url: string;
-		image: JSX.Element;
-		borders: {
-			xs: string;
-			sm: string;
-		};
-		direction: string;
-	}[];
-};
-
-export default ({ givenCustomers, ...props }: Props) => {
-	const customers = givenCustomers ? givenCustomers : generateCustomers();
 
 	return (
-		<Section style={{ overflow: 'hidden' }}>
-			<div className="container mx-auto xl:px-12">
-				<div className="flex flex-col mb-8">
-					<FloatUp>
-						<Caption className="text-center">Kunder</Caption>
-					</FloatUp>
-					<div className="flex flex-row justify-center">
-						<AnimatedH2 direction="left">Ett urval av</AnimatedH2>
-						<AnimatedH2 direction="right">
-							&nbsp;våra <Span secondary> kunder</Span>
-						</AnimatedH2>
-					</div>
-					<div className="grid grid-cols-2 lg:grid-cols-4 mt-8 mx-8 xl:mx-12">
-						{customers.map((customer, idx) => {
-							const classes = `${customer.borders.xs} ${customer.borders.sm} border-gray-300 py-2 lg:py-4 overflow-hidden`;
-
-							return (
-								<div className={classes}>
-									<FloatInDir
-										direction={customer.direction}
-										delay={idx * 0.075}
-									>
-										<ImageDiv>
-											<a href={customer.url} target="_blank">
-												{customer.image}
-											</a>
-										</ImageDiv>
-									</FloatInDir>
-								</div>
-							);
-						})}
-					</div>
-				</div>
-			</div>
-		</Section>
+		<>
+			<Customers givenCustomers={customers} />
+		</>
 	);
 };
+
+export default ClientCustomers;
