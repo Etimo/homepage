c8df8718bc6971945bb43309b89ddad67ec34151
Niclas Lindstedt <nicl@slindstedt.se>
2020-11-05 16:32:21 +0100

    Add proper 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
-
+import styled from 'styled-components';
+import tw from 'twin.macro';
+import { FadeIn } from '../animations';
 import Layout from '../components/Layout';
-import SEO from '../components/Seo';
+import Section from '../components/Section';
+import { Caption, Span } from '../elements';
+import H2 from '../elements/H2';
+
+const EmphasizedH2 = styled(H2)`
+	line-height: 1.25;
+	font-weight: 400;
+	margin-bottom: 0;
+	${tw`text-center lg:text-left md:mt-4 lg:mt-6 xl:mt-8`}
+`;
 
 const NotFoundPage = () => (
 	<Layout>
-		{/* <SEO title="404: Not found" /> */}
-		<h1>NOT FOUND</h1>
-		<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+		<Section style={{ backgroundColor: '#FFFFFF' }}>
+			<div className="flex container flex-col md:flex-row px-8 lg:px-32">
+				<div className="md:w-1/2">
+					<FadeIn direction="left" flex flexDirection="column">
+						<Caption className="text-center lg:text-left">
+							404
+						</Caption>
+						<EmphasizedH2>
+							<Span secondary>Sidan</Span> kunde inte<br />
+							hittas
+						</EmphasizedH2>
+					</FadeIn>
+				</div>
+			</div>
+		</Section>
 	</Layout>
 );
 
