a7a30268dcac051a91be13424b45d387b3524355
klaradj <73792513+klaradj@users.noreply.github.com>
2024-09-13 14:01:24 +0200

    fix: removed hover effect on mobile (#212)

--- src/animations/CyanHover.tsx
+++ src/animations/CyanHover.tsx
@@ -1,5 +1,6 @@
 import { motion, Variants } from 'framer-motion';
 import React from 'react';
+import useIsMobile from '../hooks/useIsMobile';
 
 type Props = {
 	children: React.ReactNode;
@@ -8,15 +9,20 @@ type Props = {
 };
 
 export default ({ children, className, ...props }: Props) => {
+	const isMobile = useIsMobile();
 	return (
 		<motion.div
 			className={className}
 			{...props}
-			whileHover={{
-				backgroundColor: '#42c8ad',
-				boxShadow: '0px 5px 10px 1px rgba(0,0,0,0.2)',
-				borderColor: '#42c8ad',
-			}}
+			whileHover={
+				isMobile
+					? {}
+					: {
+							backgroundColor: '#42c8ad',
+							boxShadow: '0px 5px 10px 1px rgba(0,0,0,0.2)',
+							borderColor: '#42c8ad',
+					  }
+			}
 			transition={{
 				ease: 'easeOut',
 				duration: 0.3,
