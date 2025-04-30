857e8c795cce9d80756da70e2da7a412b958ca23
Saga Swahn <sagaswahn@hotmail.com>
2025-04-30 15:38:40 +0200

    chore: updates

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import tw from 'twin.macro';
 import DashedP from './DashedP';
+import { FloatUp } from '../animations';
+import { AnimatedH2, Caption, Span } from '../elements';
 
 type Props = {
 	sectionHeight: number;
@@ -16,10 +18,6 @@ const Wrapper = styled.div`
 	${tw`flex my-auto flex-col mx-4 max-w-[700px] text-center -translate-y-[40%] md:-translate-y-[20%]`}
 `;
 
-const Heading = styled.h1`
-	${tw`font-quest text-4xl md:text-5xl xl:text-6xl leading-tight overflow-hidden break-words hyphens-auto`}
-`;
-
 const CyanText = styled.span`
 	${tw`text-cyan`}
 `;
@@ -28,14 +26,20 @@ const Splash = ({ sectionHeight }: Props) => {
 	return (
 		<Container style={{ height: sectionHeight }}>
 			<Wrapper>
-				<Heading>
-					Konsulter som bygger{' '}
-					<CyanText>bättre mjukvara för en bättre värld</CyanText>
-				</Heading>
+				<FloatUp>
+					<Caption>Konsulter som levererar</Caption>
+				</FloatUp>
+
+				<AnimatedH2 direction="left" className="text-4xl">
+					Bättre mjukvara
+				</AnimatedH2>
+				<AnimatedH2 direction="right" className="text-4xl">
+					<CyanText>för en bättre värld</CyanText>
+				</AnimatedH2>
 				<DashedP className="mt-10">
-					Etimo är konsultbolaget med handplockad senior expertis inom
-					utveckling och produkt. Vi omvandlar komplexa tekniska utmaningar till
-					lösningar som driver tillväxt, effektivitet och konkurrenskraft.
+					Etimo är konsultbolaget med handplockad expertis inom utveckling och
+					produkt som omvandlar komplexa tekniska utmaningar till hållbara
+					affärsresultat och som bidrar till att göra samhället bättre.
 				</DashedP>
 			</Wrapper>
 		</Container>
